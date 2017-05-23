97e3d78abfb5b4be703cafbdc8eab598ca490e84
Vibhum Bhardwaj <vibhumbhardwaj@hotmail.com>
2017-05-23 17:14:48 +0530

    changes done.

--- mongoclient.js
+++ mongoclient.js
@@ -1,22 +1,29 @@
 var mongomodel = require('./mongodb.js');
 
-var AngelsAndDemons = new mongomodel.Book({
+var newBook = new mongomodel.Book({
     book: 'killing floor',
     author: 'Lee Child',
     who_has_this: null
 });
 
 /*
-AngelsAndDemons.save(function(err){
+newBook.save(function(err){
     if(err) console.error('shit happens. this time happened while saving the book you gave me save into the database.');
     console.log('o ya.');
 });
 */
 
-var getBooks = mongomodel.Book.find(function(err,data){
-    if (err) console.error("Shit happens. this time while retrieving current books from database.");
-    console.log('Books:\n' + data);
-});
+var getBooks = function () {
+    mongomodel.Book.find({ _id : '5923e920d266fb2134746867' }, function (err, data) {
+        if (err) console.error("Shit happens. this time while retrieving current books from database.");
+        console.log('Books:\n' + data[0].book);
+    });
+}
+getBooks();
+
+
+/* Apparently, you cannot set the data which is not present in the schema So this saves nothing but _id and _v
+
 
 var notBook = new mongomodel.Book({
     name: "demons",
@@ -26,4 +33,4 @@ var notBook = new mongomodel.Book({
 notBook.save(function(err){
     if(err) console.error('well sorry.');
     getBooks();
-})
+})*/
