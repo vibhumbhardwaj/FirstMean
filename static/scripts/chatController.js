ed77b8cb05ebcb903cd60f78d45d4cda1c6bead3
Bhardwaj <vibhum.bhardwaj@fidelity.co.in>
2017-07-19 16:41:12 +0530

    chat initial commit

---
+++ static/scripts/chatController.js
@@ -0,0 +1,29 @@
+app.controller('chatController', function ($rootScope, $scope) {
+
+    $scope.messages = []
+
+    var socket = io.connect();
+    $scope.send = function () {
+        if($scope.message)
+            socket.emit('chat message', $scope.message);
+        $scope.messages.push($scope.message);
+        $scope.message = "";
+    }
+    socket.on('chat message', function (msg) {
+      /*  var li = document.createElement("li");
+        li.appendChild(document.createTextNode(msg));
+        document.getElementById("messages").appendChild(li);*/
+        console.log('message received.');
+    });
+
+    socket.on('newAddition', function(msg) {
+        $scope.messages.push(msg);
+        $scope.$apply();
+        window.alert('new Message!');
+    })
+
+    socket.on('update', function(msgs) {
+        $scope.messages = msgs;
+        $scope.$apply();
+    });
+});
